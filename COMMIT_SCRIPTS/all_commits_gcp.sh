507554cdad49fb25cad50f9378a62f14ab4c9ad6
Jeff Coggshall <jeff.coggshall@macys.com>
2019-10-22 15:18:20 -0700

    automated commit from laptop Tue Oct 22 15:18:09 PDT 2019

--- COMMIT_SCRIPTS/all_commits_gcp.sh
+++ COMMIT_SCRIPTS/all_commits_gcp.sh
@@ -90,5 +90,15 @@ git commit -m "automated commit from GCP $NOW"
 git push
 echo "Finished utils_and_queries"
 
+echo "Starting git_pull_only"
+cd ../git_pull_only
+git config credential.helper store
+git config --global submodule.recurse true
+git pull
+git add .
+git commit -m "automated commit from laptop $NOW"
+git push
+echo "Finished git_pull_only"
+
 
 echo "Finished all commits"
