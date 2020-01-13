430194274e7182214d3b913046d9af0c6589cc5d
Jeff Coggshall <jeff.coggshall@macys.com>
2020-01-13 10:34:45 -0800

    automated commit from laptop Mon Jan 13 10:34:41 PST 2020

--- COMMIT_SCRIPTS/all_commits_mldev.sh
+++ COMMIT_SCRIPTS/all_commits_mldev.sh
@@ -1,4 +1,5 @@
 NOW=$(date)
+THIS_DIR=$(pwd)
 
 cd /home/jovyan/work/a091569/
 git config credential.helper store
@@ -8,7 +9,7 @@ echo "Starting FBT"
 cd FBT
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished FBT"
 
@@ -16,7 +17,7 @@ echo "Starting DTOOLS"
 cd ../DTOOLS
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished DTOOLS"
 
@@ -24,7 +25,7 @@ echo "Starting BASHTOOLS"
 cd ../BASHTOOLS
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished BASHTOOLS"
 
@@ -32,7 +33,7 @@ echo "Starting nips2021"
 cd ../nips2021
 git pull
 git add .
-git commit -m "auto commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished nips2021"
 
@@ -40,7 +41,7 @@ echo "Starting FinStats"
 cd ../FinStats
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished FinStats"
 
@@ -48,7 +49,7 @@ echo "Starting Recom_Eval"
 cd ../Recom_Eval
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished Recom_Eval"
 
@@ -56,7 +57,7 @@ echo "Starting ToolsUtils"
 cd ../ToolsUtils
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished ToolsUtils"
 
@@ -64,7 +65,7 @@ echo "Starting Viz_Model_Eval"
 cd ../Viz_Model_Eval
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished Viz_Model_Eval"
 
@@ -72,7 +73,7 @@ echo "Starting comp_serve_v2"
 cd ../comp_serve_v2
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished comp_serve_v2"
 
@@ -80,7 +81,7 @@ echo "Starting complementary_product_recs"
 cd ../complementary_product_recs
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished complementary_product_recs"
 
@@ -88,7 +89,7 @@ echo "Starting COMPLEMENTARY_V2"
 cd ../COMPLEMENTARY_V2
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished COMPLEMENTARY_V2"
 
@@ -96,7 +97,7 @@ echo "Starting data_wrangling"
 cd ../data_wrangling
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished data_wrangling"
 
@@ -104,31 +105,31 @@ echo "Starting lessons"
 cd ../lessons
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished lessons"
 
-echo "Starting pyspark_modeling"
-cd ../pyspark_modeling
-git pull
-git add .
-git commit -m "automated commit from mldev $NOW"
-git push
-echo "Finished pyspark_modeling"
+# echo "Starting pyspark_modeling"
+# cd ../pyspark_modeling
+# git pull
+# git add .
+# git commit -m "automated commit from mldev $NOW"
+# git push
+# echo "Finished pyspark_modeling"
 
-echo "Starting sota_arxiv_query"
-cd ../sota_arxiv_query
-git pull
-git add .
-git commit -m "automated commit from mldev $NOW"
-git push
-echo "Finished sota_arxiv_query"
+# echo "Starting sota_arxiv_query"
+# cd ../sota_arxiv_query
+# git pull
+# git add .
+# git commit -m "automated commit from mldev $NOW"
+# git push
+# echo "Finished sota_arxiv_query"
 
 echo "Starting cython_home"
 cd ../cython_home
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished cython_home"
 
@@ -136,7 +137,7 @@ echo "Starting utils_and_queries"
 cd ../utils_and_queries
 git pull
 git add .
-git commit -m "automated commit from mldev $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished utils_and_queries"
 
@@ -145,7 +146,7 @@ cd ../git_pull_only
 git config --global submodule.recurse true
 git pull
 git add .
-git commit -m "automated commit from laptop $NOW"
+git commit -m "automated commit at $NOW from $THIS_DIR"
 git push
 echo "Finished git_pull_only"
 
