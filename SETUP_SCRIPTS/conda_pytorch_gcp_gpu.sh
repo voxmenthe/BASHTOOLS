8dc381691ac709ec5352f176a8594a79dd54c716
Jeff Coggshall <jeff.coggshall@macys.com>
2020-02-02 13:58:45 -0800

    automated commit from laptop Sun Feb  2 13:58:39 PST 2020

--- SETUP_SCRIPTS/conda_pytorch_gcp_gpu.sh
+++ SETUP_SCRIPTS/conda_pytorch_gcp_gpu.sh
@@ -3,9 +3,18 @@ sudo apt upgrade --yes
 sudo apt-get install wget --yes
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 sh ~/miniconda.sh -f -y
+
 conda install -y conda-forge pillow==6.2.1
 conda install -y anaconda pydot
 conda install -y anaconda argparse
 conda install -y anaconda tqdm
 conda install -y anaconda fastai
 
+
+beautifulsoup4 bottleneck matplotlib numexpr numpy>=1.15 packaging pandas pyyaml requests scipy spacy>=2.0.18 pillow==6.2.1
+
+
+pynvx>=1.0.0
+dataclasses
+fastprogress>=0.2.1
+nvidia-ml-py3
